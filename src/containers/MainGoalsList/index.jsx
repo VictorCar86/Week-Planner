e3f9116e197a803b831b019c3299470ffb7b45dd
Victor Carreno <vmanuelcar@hotmail.com>
2023-04-17 13:15:24 -0500

    Add modals to create goals and tasks

---
+++ src/containers/MainGoalsList/index.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { RiMore2Fill } from 'react-icons/ri';
+import './index.scss';
+
+const MainGoalsList = () => {
+    const [goalsArray, setGoalArray] = useState([]);
+
+    useEffect(() => {
+        const fetchOptions = {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        };
+
+        fetch('http://localhost:8787/api/v1/goals', fetchOptions)
+            .then(response => response.json())
+            .then(response => setGoalArray(response))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <ul className='main-goals-list-container'>
+            {goalsArray.map((goal, index) => (
+                <li className="main-goals-list-container__item" key={index}>
+                    <header>
+                        <span>{ goal.name }</span>
+
+                        <button type='button'>
+                            <RiMore2Fill />
+                        </button>
+                    </header>
+
+                    <p className='item__description'>{ goal.description }</p>
+
+                    <p>{ goal.limitDate }</p>
+
+                    <p>ID: { goal.id }</p>
+
+                    <p>30 Tasks</p>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default MainGoalsList;
--- src/routes/ListGoalsPage/index.jsx
+++ src/routes/ListGoalsPage/index.jsx
@@ -1,29 +1,49 @@
 import React from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
+import { monthNames } from '../../utils/weekDate';
+import MainGoalsList from '../../containers/MainGoalsList';
 import './index.scss';
 
 const ListGoalsPage = () => {
-  return (
-    <>
-      <main>
-        <section className='noGoalsSection'>
-            <h1>🎯 Your Goals</h1>
+    const currentDate = new Date();
+    const monthName = Object.keys(monthNames)[currentDate.getMonth()];
+    const stringDate = `${currentDate.getDate()} ${monthName} ${currentDate.getFullYear()}`;
 
-            <div className='noGoalsSection__notFoundContainer'>
-                <img
-                    src="https://images.unsplash.com/photo-1471970394675-613138e45da3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                    alt="Notes"
-                />
-                <p>There's no goals scheduled yet</p>
+    return (
+        <>
+        <main>
+            <section className='goals-section'>
+                <header className='goals-section__header'>
+                    <span>
+                        <h1>Your Goals 🎯</h1>
+                        <p>{stringDate}</p>
+                    </span>
 
-                <a href='/goals/create'>
-                    <span>+</span>
-                    <span> Create New Goal</span>
-                </a>
-            </div>
-        </section>
-      </main>
-    </>
-  )
+                    <span>
+                        <button className='goals-section__header__button' type='button'>
+                            Add New Goal
+                        </button>
+
+                        <button className='goals-section__header__button' type='button'>
+                            <BiSearchAlt />
+                        </button>
+                    </span>
+                </header>
+
+                {/* <div className='goals-section__not-found-container'>
+                    <img
+                        src="https://images.unsplash.com/photo-1471970394675-613138e45da3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                        alt="Notes"
+                    />
+                    <p>There's no goals scheduled yet</p>
+                </div> */}
+
+                <MainGoalsList />
+
+            </section>
+        </main>
+        </>
+    )
 }
 
 export default ListGoalsPage;
